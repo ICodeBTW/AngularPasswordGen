15b453db9eed79e0e828dca73134f5dccac648f5
Shreenidhi Bharadwaj H K <shreenidhihk@outlook.com>
2022-05-06 16:19:49 +0530

    Added event listner for checkboxes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,9 +9,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   password: string = '';
+  useLetters: boolean = false;
+  useNumbers: boolean = false;
+  useSymbols: boolean = false;
 
   onButtonClick() {
     this.password = 'Password';
+    console.log(this.useLetters,this.useNumbers,this.useSymbols)
+  }
+
+  onUseLettersChange() {
+    this.useLetters = !this.useLetters;
+  }
+  onUseNumbersChange() {
+    this.useNumbers = !this.useNumbers;
+  }
+  onUseSymbolsChange() {
+    this.useSymbols = !this.useSymbols;
   }
 
   getPassword() {
